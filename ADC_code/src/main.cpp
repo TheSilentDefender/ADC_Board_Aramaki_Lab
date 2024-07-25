75a0a4db6083225199dc063c1346938194b22d88
Madhav Kapa <madhavkapa16@gmail.com>
2024-07-25 02:01:12 -0400

    updated schematic to match the eval kit

--- ADC_code/src/main.cpp
+++ ADC_code/src/main.cpp
@@ -1,5 +1,4 @@
 #include <SPI.h>
-#include "DFRobot_OxygenSensor.h"
 
 // Define SPI pins
 #define CS_PIN PA_4
@@ -16,29 +15,27 @@
 SPIClass ADS7953_1;
 SPIClass ADS7953_2;
 
+
+#define CHANNELS_TO_PRINT 16
+
+int sensorData[16] = {0};
+
 void setupADS7953(SPIClass ADS7953);
 void readADS7953(SPIClass ADS7953, int adc);
+void readADS7953_teleplot(SPIClass ADS7953, int adc);
+float ADCToTemp(int adc);
+void printData();
 
 void setup()
 {
 
   // Initialize serial communication for debugging
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH);
 
-  // Set up ADS7953_1
-  ADS7953_1.begin();
-  ADS7953_1.setMOSI(MOSI_PIN_ADS7953_1);
-  ADS7953_1.setMISO(MISO_PIN_ADS7953_1);
-  ADS7953_1.setSCLK(SCK_PIN_ADS7953_1);
-  ADS7953_1.setClockDivider(SPI_CLOCK_DIV16);
-  ADS7953_1.setDataMode(SPI_MODE0); // ADS7953 uses SPI Mode 0
-  ADS7953_1.setBitOrder(MSBFIRST);  // ADS7953 expects MSB first
-  setupADS7953(ADS7953_1);
 
-  // Set up ADS7953_2
   ADS7953_2.begin();
   ADS7953_2.setMOSI(MOSI_PIN_ADS7953_2);
   ADS7953_2.setMISO(MISO_PIN_ADS7953_2);
@@ -51,31 +48,13 @@ void setup()
 
 void loop()
 {
-  if (Serial.available() > 0)
-  {
-    String input = Serial.readString();
-    if (input == "reset")
-    {
-      NVIC_SystemReset();
-    }
-    if (input == "read")
-    {
-      // read from ADS7953_1
-      // readADS7953(ADS7953_1, 0);
-
-      // read from ADS7953_2
-      for (int i = 0; i < 16; i++)
-      {
-        readADS7953(ADS7953_2, 1);
-        Serial.println();
-      }
-    }
-  }
+  readADS7953(ADS7953_2, 0);
+  printData();
+  delay(1000);
 }
 
 void setupADS7953(SPIClass ADS7953)
 {
-
   digitalWrite(CS_PIN, LOW);
   ADS7953.transfer16(0x4200); // Reset the device
   digitalWrite(CS_PIN, HIGH);
@@ -91,19 +70,34 @@ void setupADS7953(SPIClass ADS7953)
 
 void readADS7953(SPIClass ADS7953, int adc)
 {
-  digitalWrite(CS_PIN, LOW);
-  uint16_t rawData = ADS7953.transfer16(0x0000);
-  digitalWrite(CS_PIN, HIGH);
+    for (int i = 0; i < 16; i++)
+    {
+      digitalWrite(CS_PIN, LOW);
+      uint16_t rawData = ADS7953.transfer16(0x0000);
+      digitalWrite(CS_PIN, HIGH);
 
-  uint16_t channel = (rawData & 0xF000) >> 12;
-  uint16_t adcValue = rawData & 0x0FFF;
+      uint16_t channel = (rawData & 0xF000) >> 12;
+      uint16_t adcValue = rawData & 0x0FFF;
+      sensorData[channel] = adcValue;
+    }
+}
 
-  float voltage = (adcValue / 4095.0) * 5;
-
-  // Print the received data
-  Serial.print(channel + (adc * 16));
-  Serial.print(" | ");
-  Serial.print(adcValue);
-  Serial.print(" | ");
-  Serial.print(voltage, 3);
+void printData()
+{
+  sensorData[1] = sensorData[6];
+  for (int i = 0; i < CHANNELS_TO_PRINT; i++)
+  {
+    char buffer[4];
+    Serial.print("A");
+    Serial.print(i);
+    Serial.print("_");
+    sprintf(buffer, "%04d", sensorData[i]);
+    Serial.print(buffer);
+    if (i < CHANNELS_TO_PRINT - 1)
+    {
+      Serial.print("_");
+    } else {
+      Serial.println();
+    }
+  }
 }
